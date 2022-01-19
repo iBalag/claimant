10304980e110d1dcc12e9ea7b0271516c72b64b0
Ivan Balagurov <ivbalagurov@yandex.ru>
2022-01-19 21:31:47 +0300

    Improved checking logic in download_doc_handler.

--- src/bot/handlers/download_doc_handler.py
+++ src/bot/handlers/download_doc_handler.py
@@ -15,7 +15,8 @@ from docx.document import Document
 async def download_doc(message: types.Message):
     repository: Repository = Repository()
     claim_data: dict = repository.get_claim_data(message.from_user.id)
-    if not any([part_name in claim_data["claim_data"].keys() for part_name in PART_NAMES]):
+    if claim_data.get("claim_data") is None or \
+            not any([part_name in claim_data["claim_data"].keys() for part_name in PART_NAMES]):
         claim_parts_kb: ReplyKeyboardMarkup = get_claim_parts_kb(message.from_user.id)
         await message.reply("Пожалуйста, сперва заполните все разделы.", reply_markup=claim_parts_kb)
         return
