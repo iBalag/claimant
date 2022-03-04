7b36189468334c84a99deb6662f863aa2aa22bb9
Nikita Chursin <chursinn91@gmail.com>
2022-03-04 17:25:02 +0300

    tests for oof profit calc

--- src/bot/common/oof_profit_calculator_test.py
+++ src/bot/common/oof_profit_calculator_test.py
@@ -31,3 +31,32 @@ def test_calc_months_diff(start_date, end_date, month_between):
 def test_calc_work_days_in_month(start_day: int, start_day_weekday: int, end_day: int, expected_result: int):
     assert expected_result == oof_profit_calculator.calc_work_days_in_month(start_day, start_day_weekday, end_day)
 
+@pytest.mark.parametrize("start_oof_date, current_date, avr_salary, expected_profit", [
+    # (
+        # datetime(2022, 3, 4),
+        # datetime(2022, 3, 4),
+        # 20,
+        # oof_profit_calculator.OOFCalculation(
+            # oof_profit=1,
+            # oof_days=1,
+            # oof_months=0,
+            # first_month_days_oof=1,
+            # current_month_days_oof=1
+        # )
+    # ),
+    (
+        datetime(2022, 2, 4),
+        datetime(2022, 3, 4),
+        20,
+        oof_profit_calculator.OOFCalculation(
+            oof_profit=21,
+            oof_days=21,
+            oof_months=0,
+            first_month_days_oof=17,
+            current_month_days_oof=4
+        )
+    )
+])
+def test_calc_oof_profit(start_oof_date, current_date, avr_salary, expected_profit):
+    assert expected_profit == oof_profit_calculator.calc_oof_profit(start_oof_date, current_date, avr_salary)
+
